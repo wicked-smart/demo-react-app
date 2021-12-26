5309b42eba601f5a9efe144fe6b44351de5eecc2
wicked-smart <marzaanvan@gmail.com>
2021-12-26 11:30:24 +0530

    react counter app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Hello extends Component{
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
 
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
-          <h1>Hellooo {this.props.name || "Andrea" }!</h1>
-      </div>
+      <div style={{textAlign: 'center'}}>
+        <h1>{this.state.count}</h1>
+        <button style={{marginRight: '10px'}} onClick={this.increment}>increase</button>
+        <button onClick={this.decrement}>decrease</button>
+      </div>   
     );
   }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count+1
+    });
+  }
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count-1
+    });
+  }
+
   
 }   
 
-class App extends Component{
-  render(){
-    return(
-      <div>
-        <Hello name="prem"/>
-        <Hello name="Rahul"/>
-        <Hello />
-      </div>
-    );
-  }
-} 
 
 export default App;
